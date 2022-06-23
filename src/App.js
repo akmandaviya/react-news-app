1467ab808230226c89a0d9a0f332c336693600d4
AbdulKarim Mandaviya <abdulmandaviya@gmail.com>
2022-06-23 23:44:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import NavBar from './components/NavBar'
+import News from './components/News'
+import error  from  '../src/404.webp'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+
+export default class App extends Component {
+  render() {
+    return (  
+      <> <Router>
+        <NavBar/>
+
+      {/* < img style={{width:"200px", height:"200px"}} src={error} alt='error message' /> */}
+        {/* <News pageSize={6} country="in" category="sports"/> */}
+
+
+         <Routes>
+
+         <Route exact path="/" element={<News pageSize={6} country="in" category="general"/>}/>
+          <Route exact path="/buisness" element={<News pageSize={6} country="in" category="buisness"/>}/>
+          <Route exact path="/entertainment" element ={<News pageSize={6} country="in" category="entertainment"/>}/>
+          <Route exact path="/health" element={<News  pageSize={6} country="in" category="health"/>}/>
+          <Route exact path="/health" element={<News pageSize={6} country="in" category="health"/>}/>
+          <Route exact path="/science" element={<News  pageSize={6} country="in" category="science"/>}/>
+          <Route exact path="/sports" element={<News  pageSize={6} country="in" category="sports"/>}/>
+          <Route exact path="/technology" element={<News pageSize={6} country="in" category="technology"/>}/>
+
+          {/* default route  */}
+          <Route path="*"  element={<img style={{marginLeft:"200px", marginTop:"200px", width:"500px", height:"500px"}} src={error} alt='error message' />}/>
+
+          </Routes>
+        </Router>
+      </>
+    )
+  }
 }
 
-export default App;
+
---
+++ src/components/NavBar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+          <div className="container-fluid ">
+            
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse "
+              id="navbarSupportedContent"
+            >
+        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              {/* <Link className="navbar-brand" to="/"> </Link> */}
+            
+            <li className="nav-item"> <Link className="nav-link active" aria-current="page" to="/">  Home   </Link>  </li>
+                <li className="nav-item"> <Link className="nav-link" to="/about"> About </Link> </li>
+                <li className="nav-item"> <Link className="nav-link" to="/buisness"> Buisness </Link> </li>
+                <li className="nav-item"> <Link className="nav-link" to="/entertainment"> Entertainment </Link> </li>
+                <li className="nav-item"> <Link className="nav-link" to="/general">General </Link> </li>
+                <li className="nav-item"> <Link className="nav-link" to="/health"> Health </Link> </li>
+                <li className="nav-item"> <Link className="nav-link" to="/science"> Science </Link> </li>
+                <li className="nav-item"> <Link className="nav-link" to="/sports"> Sports </Link> </li>
+                <li className="nav-item"> <Link className="nav-link" to="/technology"> Tech </Link> </li>
+                 
+          </ul>
+              
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+export default NavBar;
